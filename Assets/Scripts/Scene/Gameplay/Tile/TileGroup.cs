e48fb6f7937aae5fae0a9f384ded1fcbde8083ad
Muhammad Dio Tsani Iba - 24 <dio.tsani123@gmail.com>
2022-09-05 16:51:16 +0800

    hehe

--- Assets/Scripts/Scene/Gameplay/Tile/TileGroup.cs
+++ Assets/Scripts/Scene/Gameplay/Tile/TileGroup.cs
@@ -13,10 +13,12 @@ namespace MatchPicture.Gameplay.Tile
         [SerializeField] private List<TileObject> _listTile;
         [SerializeField] private Sprite[] _sprites;
         [SerializeField] private List<Sprite> _listSprites;
+        [SerializeField] private int _firstIndex;
 
         [SerializeField] private Camera _camera;
         private int _row = 6;
-        private int _column = 5;
+        private int _column = 4;
+
         private void Awake()
         {
             _sprites = ThemeDatabase.themeDataInstance.themeSprites;
@@ -28,15 +30,25 @@ namespace MatchPicture.Gameplay.Tile
         {
             AddSprites();
         }
+        private void Update()
+        {
+            OnMouseClick();
+        }
         void CreateTile()
         {
             for (int x = 0; x < _column; x++)
             {
                 for (int y = 0; y < _row; y++)
                 {
+                    
                     TileObject tile = Instantiate(_tilePrefab, transform);
+                    
                     _listTile.Add(tile);
                     tile.transform.position = new Vector2(x, y);
+
+                    int indexName = _listTile.Count;
+                    tile.transform.name = "" + indexName;
+                    indexName++;
                 }
             }
             _camera.transform.position = new Vector3(_column / 2, _row / 2, -10f);
@@ -45,16 +57,48 @@ namespace MatchPicture.Gameplay.Tile
         void AddSprites()
         {
             int looper = _listTile.Count;
-            int index = 0; 
-            for (int i = 0; i < looper; i++)
+            if (looper % 2 != 0 || looper < 2) return;
+
+            Sprite[] sp = _sprites;
+            for (int i = 0; i < looper/2; i++)
             {
-                if(index == looper/2) // error
+                for (int j = 0; j < 2; j++)
                 {
-                    index = 0;
+                    int index = i;
+                    _listSprites.Add(sp[index]);
                 }
-                _listSprites.Add(_sprites[index]);
-                index++;
+
+                
+                //if(index == looper / 2) // error
+                //{
+                //    index = 0;
+                //}
+                //_listSprites.Add(_sprites[index]);
+                //index++;
             }
+        }
+        private void OnMouseClick()
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                Vector3 world = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                RaycastHit2D hit = Physics2D.Raycast(world, Vector2.zero);
+
+                if (hit.collider != null)
+                {
+                    ClickedSprite(hit);
+                    
+                }
+            }
+            
+        }
+        void ClickedSprite(RaycastHit2D hit2d)
+        {
+            string name = hit2d.transform.name;
+            _firstIndex = int.Parse(name);
+            _listTile[_firstIndex].GetComponent<SpriteRenderer>().sprite = _listSprites[_firstIndex];
+
+            Debug.Log("clicked");
         }
 
     }
