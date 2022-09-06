021263382ce314bf48a7c2444c60b0c3823efe77
Muhammad Dio Tsani Iba - 24 <dio.tsani123@gmail.com>
2022-09-06 09:51:01 +0800

    theme list, generate tile, click tile

--- Assets/Scripts/Scene/Gameplay/Tile/TileGroup.cs
+++ Assets/Scripts/Scene/Gameplay/Tile/TileGroup.cs
@@ -9,6 +9,7 @@ namespace MatchPicture.Gameplay.Tile
     {
 
         [SerializeField] private TileObject _tilePrefab;
+        [SerializeField] private Sprite[] spriteRenderer;
 
         [SerializeField] private List<TileObject> _listTile;
         [SerializeField] private Sprite[] _sprites;
@@ -17,18 +18,21 @@ namespace MatchPicture.Gameplay.Tile
 
         [SerializeField] private Camera _camera;
         private int _row = 6;
-        private int _column = 4;
+        private int _column = 5;
 
         private void Awake()
         {
             _sprites = ThemeDatabase.themeDataInstance.themeSprites;
             _listTile = new List<TileObject>();
             _listSprites = new List<Sprite>();
+
+            AddSprites();
             CreateTile();
+            Shuffle(_listSprites);
         }
         private void Start()
         {
-            AddSprites();
+            
         }
         private void Update()
         {
@@ -36,6 +40,7 @@ namespace MatchPicture.Gameplay.Tile
         }
         void CreateTile()
         {
+            int maxTile = _column * _row;
             for (int x = 0; x < _column; x++)
             {
                 for (int y = 0; y < _row; y++)
@@ -46,9 +51,11 @@ namespace MatchPicture.Gameplay.Tile
                     _listTile.Add(tile);
                     tile.transform.position = new Vector2(x, y);
 
-                    int indexName = _listTile.Count;
+                    int indexName = _listTile.Count-1;
                     tile.transform.name = "" + indexName;
-                    indexName++;
+
+                    
+                    //indexName++;
                 }
             }
             _camera.transform.position = new Vector3(_column / 2, _row / 2, -10f);
@@ -56,27 +63,35 @@ namespace MatchPicture.Gameplay.Tile
 
         void AddSprites()
         {
-            int looper = _listTile.Count;
+            int looper = _row * _column;
             if (looper % 2 != 0 || looper < 2) return;
 
-            Sprite[] sp = _sprites;
             for (int i = 0; i < looper/2; i++)
             {
+                Sprite randomSpites = GetRandomSprite(_sprites);
                 for (int j = 0; j < 2; j++)
                 {
-                    int index = i;
-                    _listSprites.Add(sp[index]);
+                    Sprite addSprite = randomSpites;
+                    
+                    _listSprites.Add(addSprite);
                 }
-
-                
-                //if(index == looper / 2) // error
-                //{
-                //    index = 0;
-                //}
-                //_listSprites.Add(_sprites[index]);
-                //index++;
             }
         }
+        Sprite GetRandomSprite(Sprite[] sprites)
+        {
+            return sprites[Random.Range(0,sprites.Length)];
+        }
+        void Shuffle(List<Sprite> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Sprite temp = list[i];
+                int randomIndex = Random.Range(i, list.Count);
+                list[i] = list[randomIndex];
+                list[randomIndex] = temp;
+            }
+        }
+
         private void OnMouseClick()
         {
             if(Input.GetMouseButtonDown(0))
@@ -86,6 +101,7 @@ namespace MatchPicture.Gameplay.Tile
 
                 if (hit.collider != null)
                 {
+                    Debug.Log(hit.collider.gameObject.name);
                     ClickedSprite(hit);
                     
                 }
@@ -96,9 +112,10 @@ namespace MatchPicture.Gameplay.Tile
         {
             string name = hit2d.transform.name;
             _firstIndex = int.Parse(name);
-            _listTile[_firstIndex].GetComponent<SpriteRenderer>().sprite = _listSprites[_firstIndex];
-
-            Debug.Log("clicked");
+            
+            //_listTile[_firstIndex].GetComponent<SpriteRenderer>().sprite = _listSprites[_firstIndex];
+            _listTile[_firstIndex].transform.GetChild(0).gameObject.SetActive(true);
+            _listTile[_firstIndex].transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = _listSprites[_firstIndex];
         }
 
     }
